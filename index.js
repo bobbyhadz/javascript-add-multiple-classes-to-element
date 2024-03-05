97fd13a5006d4efa5c64edb8b35184536b10d123
bobbyhadz <opmftw@gmail.com>
2024-03-05 06:58:02 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log('bobbyhadz.com');
+
+const box = document.getElementById('box');
+
+// ✅ Add multiple classes
+box.classList.add('bg-blue', 'text-white');
+
+// ✅ Remove multiple classes
+box.classList.remove('first-class', 'second-class');
